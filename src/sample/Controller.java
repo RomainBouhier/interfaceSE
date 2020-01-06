ccee67ddf4c72df39dd36c814148d8fa7c7e7faf
romicron <roro.bouhier@gmail.com>
2020-01-06 17:31:15 +0100

    puqh

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,10 +3,67 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Label;
 
+
 public class Controller {
     public Label helloWorld;
 
     public void sayHelloWorld(ActionEvent actionEvent) {
         helloWorld.setText("Hello World!");
+    }
+
+    public void AjouterNoeud(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void SupprimerNoeud(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void AjouterArc(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void SupprimerArc(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void AjouterPaquet(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void SupprimerPaquet(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void AfficherDetail(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void Navigation(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void Zoom(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void ReseauRoutier(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void ReseauSocial(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void ReseauWeb(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void Reset(ActionEvent actionEvent) {
+        System.out.println("TODO");
+    }
+
+    public void FluxAleatoire(ActionEvent actionEvent) {
+        System.out.println("TODO");
     }
 }
